d5b1cccb3f5278d7af15f5024e18d858fb9aeb1d
Anna Smolentzov <annasmolentzov@gmail.com>
2019-01-22 20:20:19 -0700

    Updates SearchController show method to check search params

--- app/controllers/api/v1/customers/search_controller.rb
+++ app/controllers/api/v1/customers/search_controller.rb
@@ -1,7 +1,11 @@
 class Api::V1::Customers::SearchController < ApplicationController
   
   def show
-    customer = Customer.find(params[:id])
+    if params[:id]
+      customer = Customer.find(params[:id])
+    elsif params[:first_name]
+      customer = Customer.find_by(first_name: params[:first_name])
+    end
     render json: customer
   end
   
