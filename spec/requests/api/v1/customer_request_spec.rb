e61e555de0f21ca985956ad1b20bfa2f4343e80e
Anna Smolentzov <annasmolentzov@gmail.com>
2019-01-22 19:29:30 -0700

    Adds test for customers show

--- spec/requests/api/v1/customer_request_spec.rb
+++ spec/requests/api/v1/customer_request_spec.rb
@@ -11,4 +11,16 @@ describe 'Customers API' do
     customers = JSON.parse(response.body)
     expect(customers.count).to eq(3)
   end
+  
+  it 'can get a single customer by its id' do
+    customer = create(:customer)
+    
+    get "/api/v1/customers/#{customer.id}"
+    
+    returned_customer = JSON.parse(response.body)
+    
+    expect(response).to be_successful
+    expect(returned_customer["name"]).to eq(customer.name)
+    expect(returned_customer["id"]).to eq(customer.id)
+  end
 end
