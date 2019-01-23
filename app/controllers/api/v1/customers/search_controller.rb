0eee8bcc6cedc070a1761e496225c2c777a6ad49
Anna Smolentzov <annasmolentzov@gmail.com>
2019-01-23 14:50:09 -0700

    Updates search index with dates and case insensitive

--- app/controllers/api/v1/customers/search_controller.rb
+++ app/controllers/api/v1/customers/search_controller.rb
@@ -4,9 +4,15 @@ class Api::V1::Customers::SearchController < ApplicationController
     if params[:id]
       customer = Customer.where(id: params[:id])
     elsif params[:first_name]
-      customer = Customer.where(first_name: params[:first_name])
+      customer = Customer.where("first_name ILIKE ?", params[:first_name])
     elsif params[:last_name]
-      customer = Customer.where(last_name: params[:last_name])
+      customer = Customer.where("last_name ILIKE ?", params[:last_name])
+    elsif params[:created_at]
+      created_at = Time.parse(params[:created_at])
+      customer = Customer.where("created_at BETWEEN ? AND ?", created_at.beginning_of_minute, created_at.end_of_minute)
+    elsif params[:updated_at]
+      updated_at = Time.parse(params[:updated_at])
+      customer = Customer.where("updated_at BETWEEN ? AND ?", updated_at.beginning_of_minute, updated_at.end_of_minute)
     end
     render json: CustomerSerializer.new(customer)
   end
