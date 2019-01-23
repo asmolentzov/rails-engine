5cf62f7dcf0393a4f545b15875aa8ac9c6c89a35
Anna Smolentzov <annasmolentzov@gmail.com>
2019-01-22 20:33:59 -0700

    Updates search show method with more parameters

--- app/controllers/api/v1/customers/search_controller.rb
+++ app/controllers/api/v1/customers/search_controller.rb
@@ -5,6 +5,12 @@ class Api::V1::Customers::SearchController < ApplicationController
       customer = Customer.find(params[:id])
     elsif params[:first_name]
       customer = Customer.find_by(first_name: params[:first_name])
+    elsif params[:last_name]
+      customer = Customer.find_by(last_name: params[:last_name])
+    elsif params[:created_at]
+      customer = Customer.find_by(created_at: params[:created_at])
+    elsif params[:updated_at]
+      customer = Customer.find_by(updated_at: params[:updated_at])
     end
     render json: customer
   end
