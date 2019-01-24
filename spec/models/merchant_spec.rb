10b26727750992d4ca66f766765b8096fe927adf
Anna Smolentzov <annasmolentzov@gmail.com>
2019-01-23 17:59:56 -0700

    Adds total_revenue model test to merchant

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -4,4 +4,20 @@ RSpec.describe Merchant, type: :model do
   describe 'Validations' do
     it { should validate_presence_of(:name) }
   end
+  
+  describe 'Instance Methods' do
+    describe '#total_revenue' do
+      it 'should return the total revenue for a merchant across successful transactions' do
+        merchant = create(:merchant)
+        item = create(:item, merchant: merchant, unit_price: 10)
+        invoice = create(:invoice, merchant: merchant)
+        invoice_item = create(:invoice_item, item: item, invoice: invoice, unit_price: 10, quantity: 2)
+        transaction = create(:transaction, invoice: invoice, result: "success")
+        transaction_2 = create(:transaction, invoice: invoice, result: "fail")
+        
+        revenue = invoice_item.unit_price * invoice_item.quantity
+        expect(merchant.total_revenue).to eq(revenue)
+      end
+    end
+  end
 end
