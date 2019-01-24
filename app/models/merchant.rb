3c391f73ea4e8912dcad3f11ba0115bebb8259bf
Anna Smolentzov <annasmolentzov@gmail.com>
2019-01-23 20:23:18 -0700

    Adds merchants_by_revenue method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -9,4 +9,14 @@ class Merchant < ApplicationRecord
             .where(transactions: {result: "success"})
             .sum("invoice_items.quantity * invoice_items.unit_price")
   end
+  
+  def self.merchants_by_revenue(quantity)
+     Merchant.joins(invoices: :invoice_items)
+              .joins(invoices: :transactions)
+              .where(transactions: {result: "success"})
+              .select("merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS total_revenue")
+              .group(:id)
+              .order("total_revenue DESC")
+              .limit(quantity)
+  end
 end
