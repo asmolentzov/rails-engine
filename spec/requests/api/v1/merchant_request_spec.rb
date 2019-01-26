4f69162b3002fb5e1ac846034bacbe39d5f44cd1
Anna Smolentzov <annasmolentzov@gmail.com>
2019-01-26 13:16:34 -0700

    Adds test for merchant items

--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -111,6 +111,25 @@ describe 'Merchants API' do
     expect(returned_merchant.count).to eq(1)
   end
   
+  describe 'Relationships' do
+    it 'returns a collection of items associated with a merchant' do
+      merchant = create(:merchant)
+      item_1 = create(:item, merchant: merchant)
+      item_2 = create(:item, merchant: merchant)
+      item_3 = create(:item, merchant: merchant)
+      item_4 = create(:item)
+      
+      get "/api/v1/merchants/#{merchant.id}/items"
+      
+      expect(response).to be_successful
+      returned_items = JSON.parse(response.body)["data"]
+      expect(returned_items.count).to eq(3)
+      expect(returned_items.first["attributes"]["id"]).to eq(item_1.id)
+      expect(returned_items.second["attributes"]["id"]).to eq(item_2.id)
+      expect(returned_items.last["attributes"]["id"]).to eq(item_3.id)
+    end
+  end
+  
   describe 'Business Intelligence' do
     it 'returns the total revenue for a specified merchant across successful transactions' do
       merchant = create(:merchant)
