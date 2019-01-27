2095e1933cfd9cf37f503c4ce3f908c069918e01
Anna Smolentzov <annasmolentzov@gmail.com>
2019-01-26 17:25:09 -0700

    Adds tests for invoice items index and show

---
+++ spec/requests/api/v1/invoice_item_request_spec.rb
@@ -0,0 +1,35 @@
+'rails_helper'
+
+describe 'Invoice Items API' do
+  describe 'for a collection of invoice items' do
+    before(:each) do
+      create_list(:invoice_item, 3)
+    end
+    
+    it 'can return all the invoice items' do
+      get '/api/v1/invoice_items' 
+      
+      expect(response).to be_successful
+      
+      returned_invoice_items = JSON.parse(response.body)["data"]
+      expect(returned_invoice_items.count).to eq(3)
+      expect(returned_invoice_items.first["attributes"]["type"]).to eq("invoice_item")
+    end
+  end
+  
+  describe 'for a single invoice item' do
+    before(:each) do
+      @invoice_item = create(:invoice_item)
+    end
+    
+    it 'can return a single invoice item' do
+      get "/api/v1/invoice_items/#{@invoice_item.id}"
+    end
+    
+    after(:each) do
+      expect(response).to be_successful
+      returned_invoice_item = JSON.parse(response.body)
+      expect(returned_invoice_item["attributes"]["id"]).to eq(@invoice_item.id)
+    end
+  end
+end
