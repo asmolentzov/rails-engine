e82dfc9f1ffeac8e89fc9741e7ab3a21d897f6b0
Anna Smolentzov <annasmolentzov@gmail.com>
2019-01-27 16:35:10 -0700

    Adds find all for transactions by attributes

--- spec/requests/api/v1/transaction_request_spec.rb
+++ spec/requests/api/v1/transaction_request_spec.rb
@@ -15,6 +15,85 @@ describe 'Transaction API' do
       expect(returned_transactions.count).to eq(3)
       expect(returned_transactions.first["type"]).to eq("transaction")
     end
+    it 'can find all transactions by id' do
+      transaction = create(:transaction)
+      
+      get "/api/v1/transactions/find_all?id=#{transaction.id}"
+      
+      expect(response).to be_successful
+      
+      returned_transactions = JSON.parse(response.body)["data"]
+      expect(returned_transactions.count).to eq(1)
+      expect(returned_transactions.first["attributes"]["id"]).to eq(transaction.id)
+    end
+    it 'can find all transactions by invoice_id' do
+      invoice = create(:invoice)
+      transaction = create(:transaction, invoice: invoice)
+      transaction_2 = create(:transaction, invoice: invoice)
+      
+      get "/api/v1/transactions/find_all?invoice_id=#{transaction.invoice_id}"
+      
+      expect(response).to be_successful
+      
+      returned_transactions = JSON.parse(response.body)["data"]
+      expect(returned_transactions.count).to eq(2)
+      expect(returned_transactions.first["attributes"]["id"]).to eq(transaction.id)
+      expect(returned_transactions.last["attributes"]["id"]).to eq(transaction_2.id)
+    end
+    it 'can find all transactions by credit_card_number' do
+      transaction = create(:transaction, credit_card_number: "541
+      6515")
+      transaction_2 = create(:transaction, credit_card_number: "541
+      6515")
+      
+      get "/api/v1/transactions/find_all?credit_card_number=#{transaction.credit_card_number}"
+      
+      expect(response).to be_successful
+      
+      returned_transactions = JSON.parse(response.body)["data"]
+      expect(returned_transactions.count).to eq(2)
+      expect(returned_transactions.first["attributes"]["id"]).to eq(transaction.id)
+      expect(returned_transactions.last["attributes"]["id"]).to eq(transaction_2.id)
+    end
+    it 'can find all transactions by result' do
+      transaction = create(:transaction, result: 'success')
+      transaction_2 = create(:transaction, result: 'success')
+      
+      get "/api/v1/transactions/find_all?result=#{transaction.result}"
+      
+      expect(response).to be_successful
+      
+      returned_transactions = JSON.parse(response.body)["data"]
+      expect(returned_transactions.count).to eq(2)
+      expect(returned_transactions.first["attributes"]["id"]).to eq(transaction.id)
+      expect(returned_transactions.last["attributes"]["id"]).to eq(transaction_2.id)
+    end
+    it 'can find all transactions by created_at' do
+      transaction = create(:transaction, created_at: "2012-03-27 14:54:10 UTC")
+      transaction_2 = create(:transaction, created_at: "2012-03-27 14:54:10 UTC")
+      
+      get "/api/v1/transactions/find_all?created_at=#{transaction.created_at}"
+      
+      expect(response).to be_successful
+      
+      returned_transactions = JSON.parse(response.body)["data"]
+      expect(returned_transactions.count).to eq(2)
+      expect(returned_transactions.first["attributes"]["id"]).to eq(transaction.id)
+      expect(returned_transactions.last["attributes"]["id"]).to eq(transaction_2.id)
+    end
+    it 'can find all transactions by updated_at' do
+      transaction = create(:transaction, updated_at: "2012-03-27 14:54:10 UTC")
+      transaction_2 = create(:transaction, updated_at: "2012-03-27 14:54:10 UTC")
+      
+      get "/api/v1/transactions/find_all?updated_at=#{transaction.updated_at}"
+      
+      expect(response).to be_successful
+      
+      returned_transactions = JSON.parse(response.body)["data"]
+      expect(returned_transactions.count).to eq(2)
+      expect(returned_transactions.first["attributes"]["id"]).to eq(transaction.id)
+      expect(returned_transactions.last["attributes"]["id"]).to eq(transaction_2.id)
+    end
   end
   
   describe 'for a single transaction' do
