021846677eed573e34cddca668c9990daab1edd4
Anna Smolentzov <annasmolentzov@gmail.com>
2019-01-26 17:37:07 -0700

    Adds tests for finding single invoice items by attributes

--- spec/requests/api/v1/invoice_item_request_spec.rb
+++ spec/requests/api/v1/invoice_item_request_spec.rb
@@ -25,6 +25,27 @@ describe 'Invoice Items API' do
     it 'can return a single invoice item' do
       get "/api/v1/invoice_items/#{@invoice_item.id}"
     end
+    it 'can find an invoice item by id' do
+      get "/api/v1/invoice_items/find?id=#{@invoice_item.id}"
+    end
+    it 'can find an invoice item by item_id' do
+      get "/api/v1/invoice_items/find?item_id=#{@invoice_item.item_id}"
+    end
+    it 'can find an invoice item by invoice_id' do
+      get "/api/v1/invoice_items/find?invoice_id=#{@invoice_item.invoice_id}"
+    end
+    it 'can find an invoice item by quantity' do
+      get "/api/v1/invoice_items/find?quantity=#{@invoice_item.quantity}"
+    end
+    it 'can find an invoice item by unit_price' do
+      get "/api/v1/invoice_items/find?unit_price=#{@invoice_item.unit_price}"
+    end
+    it 'can find an invoice item by created_at' do
+      get "/api/v1/invoice_items/find?created_at=#{@invoice_item.created_at}"
+    end
+    it 'can find an invoice item by updated_at' do
+      get "/api/v1/invoice_items/find?updated_at=#{@invoice_item.updated_at}"
+    end
     
     after(:each) do
       expect(response).to be_successful
